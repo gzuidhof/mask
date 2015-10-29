c134c33c49d3f4cd467de298abdde1be7df8829f
Guido <me@guido.io>
2015-10-29 14:50:29 +0100

    Question radio box question, new face images

--- client/views/question.js
+++ client/views/question.js
@@ -6,23 +6,39 @@
       var questionNumber = template.data.questionNumber;
       var question = questionOrder(Session.get('participantId'))[questionNumber];
 
-      var response = {
-        participantId: Session.get('participantId'),
-        questionNumber: questionNumber,
-        likability: event.target.likability.value,
-      }
+      var element = template.find('input:radio[name=lika]:checked');
 
-      for (var key in question) { //Add nose eyes etc.
-        if (question.hasOwnProperty(key)) {
-            response[key] = question[key];
+      if (!element) {
+          template.find("#error-text").innerHTML = "Please select a value";
+      }
+      else {
+        template.find("#error-text").innerHTML = "";
+        var likability = element.value;
+        element.checked = false;
+        element.parentElement.className = 'btn btn-default';
+
+        var response = {
+          participantId: Session.get('participantId'),
+          questionNumber: questionNumber,
+          likability: likability,
         }
+
+
+
+        for (var key in question) { //Add nose eyes etc.
+          if (question.hasOwnProperty(key)) {
+              response[key] = question[key];
+          }
+        }
+
+
+        console.log("Submitting response", response);
+        Responses.insert(response);
+        Session.setPersistent('currentQuestionNumber', questionNumber+1);
       }
 
 
-      console.log("Submitting response", response);
-      Responses.insert(response);
 
-      Session.setPersistent('currentQuestionNumber', questionNumber+1);
 
       return false;
     }
